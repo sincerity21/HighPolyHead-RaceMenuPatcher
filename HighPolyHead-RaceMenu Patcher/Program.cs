4708ef75bde2ada7cecb9154c183320e55816a28
sincerity2103 <sincerity2103@gmail.com>
2026-01-09 18:39:48 +0800

    Updated.

--- HighPolyHead-RaceMenu Patcher/Program.cs
+++ HighPolyHead-RaceMenu Patcher/Program.cs
@@ -7,7 +7,6 @@ using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Plugins.Cache;
-using Mutagen.Bethesda.Plugins.Records;
 using System.Threading.Tasks;
 using Noggog;
 
@@ -35,140 +34,70 @@ namespace HighPolyHeadUpdateRaces
             Console.WriteLine("Building vanilla to HPH head part map...");
             var vanillaToHphParts = BuildVanillaToHphMap(state);
 
-            Console.WriteLine("Analyzing load order to determine required patches...");
-            var (racesToPatch, npcsToPatch) = PrePatch(state, vanillaToHphParts);
-
             var masters = new HashSet<ModKey>(state.PatchMod.ModHeader.MasterReferences.Select(x => x.Master));
             var raceHeadPartsMale = new Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>>();
             var raceHeadPartsFemale = new Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>>();
-
+            
             Console.WriteLine("Patching races...");
-            foreach (var race in racesToPatch)
+            foreach (var raceRecord in state.LoadOrder.PriorityOrder.OnlyEnabled().Race().WinningOverrides())
             {
-                var newMasters = GetMastersFor(race, state.LinkCache);
+                if (raceRecord.EditorID == null || raceRecord.HeadData == null) continue;
+
+                var needsPatching = raceRecord.HeadData.Male?.HeadParts.Any(h => vanillaToHphParts.ContainsKey(h.Head)) == true ||
+                                    raceRecord.HeadData.Female?.HeadParts.Any(h => vanillaToHphParts.ContainsKey(h.Head)) == true;
+
+                if (!needsPatching) continue;
+                
+                var newMasters = GetMastersFor(raceRecord, state.LinkCache);
                 var newMasterCount = newMasters.Count(m => !masters.Contains(m));
 
                 if (masters.Count + newMasterCount > 252)
                 {
-                    Console.WriteLine($"Master limit reached. Skipping remaining plugins. Last plugin considered: {race.FormKey.ModKey}");
+                    Console.WriteLine($"Master limit reached. Skipping remaining plugins. Last plugin considered: {raceRecord.FormKey.ModKey}");
                     break;
                 }
                 masters.UnionWith(newMasters);
-                PatchRace(race, state, vanillaToHphParts, raceHeadPartsMale, raceHeadPartsFemale);
-            }
-
-            Console.WriteLine("Patching NPCs...");
-            var patchedRaces = raceHeadPartsMale.Keys.ToHashSet();
-            patchedRaces.UnionWith(raceHeadPartsFemale.Keys);
-
-            foreach (var npc in npcsToPatch)
-            {
-                var newMasters = GetMastersFor(npc, state.LinkCache);
-                var newMasterCount = newMasters.Count(m => !masters.Contains(m));
-
-                if (masters.Count + newMasterCount > 252)
-                {
-                    Console.WriteLine($"Master limit reached. Skipping remaining plugins. Last plugin considered: {npc.FormKey.ModKey}");
-                    break;
-                }
-
-                masters.UnionWith(newMasters);
-                PatchNpc(npc, state, vanillaToHphParts, raceHeadPartsMale, raceHeadPartsFemale, patchedRaces);
-            }
-
-            Console.WriteLine("Patching complete!");
-        }
-        
-        private static HashSet<ModKey> GetMastersFor(IMajorRecordGetter record, ILinkCache linkCache)
-        {
-            var masters = new HashSet<ModKey>();
-            if (record is null) return masters;
-
-            masters.Add(record.FormKey.ModKey);
-
-            foreach (var link in record.EnumerateFormLinks())
-            {
-                if (link.FormKey.IsNull) continue;
-                masters.Add(link.FormKey.ModKey);
-            }
-
-            return masters;
-        }
-
-
-        private static void PatchRace(IRaceGetter raceRecord, IPatcherState<ISkyrimMod, ISkyrimModGetter> state, IReadOnlyDictionary<IFormLinkGetter<IHeadPartGetter>, IFormLinkGetter<IHeadPartGetter>> vanillaToHphParts, Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>> raceHeadPartsMale, Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>> raceHeadPartsFemale)
-        {
-            var raceOverride = state.PatchMod.Races.GetOrAddAsOverride(raceRecord);
-            var changed = false;
-
-            if (raceOverride.HeadData != null)
-            {
+                
+                var raceOverride = state.PatchMod.Races.GetOrAddAsOverride(raceRecord);
                 var raceFormLinkGetter = raceOverride.ToLinkGetter();
-                if (raceOverride.HeadData.Female != null)
+                
+                if (raceOverride.HeadData?.Female != null)
                 {
                     foreach (var raceHead in raceOverride.HeadData.Female.HeadParts)
                     {
-                        if (!vanillaToHphParts.TryGetValue(raceHead.Head, out var part)) continue;
-
-                        raceHeadPartsFemale.GetOrAdd(raceFormLinkGetter).Add(raceHead.Head);
-                        changed = true;
-                        raceHead.Head.SetTo(part);
+                        if (vanillaToHphParts.TryGetValue(raceHead.Head, out var part))
+                        {
+                            raceHeadPartsFemale.GetOrAdd(raceFormLinkGetter).Add(raceHead.Head.FormKey);
+                            raceHead.Head.SetTo(part);
+                        }
                     }
                 }
-                if (raceOverride.HeadData.Male != null)
+                if (raceOverride.HeadData?.Male != null)
                 {
                     foreach (var raceHead in raceOverride.HeadData.Male.HeadParts)
                     {
-                        if (!vanillaToHphParts.TryGetValue(raceHead.Head, out var part)) continue;
-
-                        raceHeadPartsMale.GetOrAdd(raceFormLinkGetter).Add(raceHead.Head);
-                        changed = true;
-                        raceHead.Head.SetTo(part);
+                        if (vanillaToHphParts.TryGetValue(raceHead.Head, out var part))
+                        {
+                            raceHeadPartsMale.GetOrAdd(raceFormLinkGetter).Add(raceHead.Head.FormKey);
+                            raceHead.Head.SetTo(part);
+                        }
                     }
                 }
             }
-        }
+            
+            var patchedRaces = raceHeadPartsMale.Keys.ToHashSet();
+            patchedRaces.UnionWith(raceHeadPartsFemale.Keys);
 
-        private static void PatchNpc(INpcGetter npcPreset, IPatcherState<ISkyrimMod, ISkyrimModGetter> state, IReadOnlyDictionary<IFormLinkGetter<IHeadPartGetter>, IFormLinkGetter<IHeadPartGetter>> vanillaToHphParts, Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>> raceHeadPartsMale, Dictionary<IFormLinkGetter<IRaceGetter>, HashSet<IFormLinkGetter<IHeadPartGetter>>> raceHeadPartsFemale, HashSet<IFormLinkGetter<IRaceGetter>> patchedRaces)
-        {
-            try
+            Console.WriteLine("Patching NPCs...");
+            foreach (var npcPreset in state.LoadOrder.PriorityOrder.OnlyEnabled().Npc().WinningOverrides())
             {
-                if (npcPreset.EditorID == null) return;
+                if (npcPreset.EditorID == null) continue;
 
                 var npcCopy = npcPreset.DeepCopy();
                 var changed = false;
 
                 var eid = npcCopy.EditorID;
                 var withoutLastTwo = (eid.Length > 2) ? eid[..^2] : eid;
-
-                if (!withoutLastTwo.EndsWith("Preset") && !npcCopy.Race.Equals(Skyrim.Race.FoxRace))
-                {
-                    if (patchedRaces.Contains(npcCopy.Race))
-                    {
-                        var npcPartTypes = new HashSet<HeadPart.TypeEnum>();
-                        foreach (var part in npcCopy.HeadParts)
-                        {
-                            if (!part.TryResolve(state.LinkCache, out var headPartGetter)) continue;
-                            if (headPartGetter.Type != null) npcPartTypes.Add((HeadPart.TypeEnum)headPartGetter.Type);
-                        }
-
-                        var raceHeadParts = npcCopy.Configuration.Flags.HasFlag(NpcConfiguration.Flag.Female)
-                            ? raceHeadPartsFemale
-                            : raceHeadPartsMale;
-
-                        if (raceHeadParts.TryGetValue(npcCopy.Race, out var currentRaceHeadParts))
-                        {
-                            foreach (var part in currentRaceHeadParts)
-                            {
-                                if (part.TryResolve(state.LinkCache, out var headPartGetter) && headPartGetter?.Type != null && !npcPartTypes.Contains((HeadPart.TypeEnum)headPartGetter.Type))
-                                {
-                                    npcCopy.HeadParts.Add(part);
-                                    changed = true;
-                                }
-                            }
-                        }
-                    }
-                }
 
                 if (withoutLastTwo.EndsWith("Preset"))
                 {
@@ -181,19 +110,65 @@ namespace HighPolyHeadUpdateRaces
                         }
                     }
                 }
+                else if (patchedRaces.Contains(npcCopy.Race))
+                {
+                    var npcPartTypes = new HashSet<HeadPart.TypeEnum>();
+                    foreach (var part in npcCopy.HeadParts)
+                    {
+                        if (part.TryResolve(state.LinkCache, out var headPartGetter) && headPartGetter.Type != null)
+                        {
+                            npcPartTypes.Add((HeadPart.TypeEnum)headPartGetter.Type);
+                        }
+                    }
+
+                    var raceHeadParts = npcCopy.Configuration.Flags.HasFlag(NpcConfiguration.Flag.Female)
+                        ? raceHeadPartsFemale
+                        : raceHeadPartsMale;
+
+                    if (raceHeadParts.TryGetValue(npcCopy.Race, out var currentRaceHeadParts))
+                    {
+                        foreach (var part in currentRaceHeadParts)
+                        {
+                            if (part.TryResolve(state.LinkCache, out var headPartGetter) && headPartGetter?.Type != null && !npcPartTypes.Contains((HeadPart.TypeEnum)headPartGetter.Type))
+                            {
+                                npcCopy.HeadParts.Add(part);
+                                changed = true;
+                            }
+                        }
+                    }
+                }
 
                 if (changed)
                 {
+                    var newMasters = GetMastersFor(npcPreset, state.LinkCache);
+                    var newMasterCount = newMasters.Count(m => !masters.Contains(m));
+
+                    if (masters.Count + newMasterCount > 252)
+                    {
+                        Console.WriteLine($"Master limit reached. Skipping remaining plugins. Last plugin considered: {npcPreset.FormKey.ModKey}");
+                        continue;
+                    }
+
+                    masters.UnionWith(newMasters);
                     state.PatchMod.Npcs.Set(npcCopy);
                 }
             }
-            catch (Exception e)
+
+            Console.WriteLine("Patching complete!");
+        }
+        
+        private static HashSet<ModKey> GetMastersFor(IMajorRecordGetter record, ILinkCache<ISkyrimMod, ISkyrimModGetter> linkCache)
+        {
+            var masters = new HashSet<ModKey> { record.FormKey.ModKey };
+            if (record is null) return masters;
+
+            foreach (var link in record.EnumerateFormLinks())
             {
-                Console.WriteLine($"Error: {e.Message}");
-                Console.WriteLine($"Error NPC: {npcPreset}");
-                Console.WriteLine($"Error NPC EditorID: {npcPreset.EditorID}");
-                Console.WriteLine($"Stack trace: {e.StackTrace}");
+                if (link.FormKey.IsNull) continue;
+                masters.Add(link.FormKey.ModKey);
             }
+
+            return masters;
         }
 
         private static Dictionary<IFormLinkGetter<IHeadPartGetter>, IFormLinkGetter<IHeadPartGetter>> BuildVanillaToHphMap(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
@@ -220,7 +195,6 @@ namespace HighPolyHeadUpdateRaces
 
                 vanillaToHphParts[vanillaHeadPart.ToLinkGetter()] = hphHeadParts[hphKey].ToLinkGetter();
 
-                // If the vanilla head part is already not playable, we assume it's already been patched.
                 if (vanillaHeadPart.Flags.HasFlag(HeadPart.Flag.Playable))
                 {
                     var gimmeHead = state.PatchMod.HeadParts.GetOrAddAsOverride(vanillaHeadPart);
@@ -229,62 +203,6 @@ namespace HighPolyHeadUpdateRaces
             }
 
             return vanillaToHphParts;
-        }
-
-        private static (List<IRaceGetter> racesToPatch, List<INpcGetter> npcsToPatch) PrePatch(
-            IPatcherState<ISkyrimMod, ISkyrimModGetter> state,
-            IReadOnlyDictionary<IFormLinkGetter<IHeadPartGetter>, IFormLinkGetter<IHeadPartGetter>> vanillaToHphParts)
-        {
-            var racesToPatch = new List<IRaceGetter>();
-            var npcsToPatch = new List<INpcGetter>();
-
-            var racesThatWillBePatched = new HashSet<IFormLinkGetter<IRaceGetter>>();
-
-            foreach (var raceRecord in state.LoadOrder.PriorityOrder.OnlyEnabled().Race().WinningOverrides())
-            {
-                if (raceRecord.EditorID == null || raceRecord.HeadData == null) continue;
-
-                var needsPatching = false;
-                if (raceRecord.HeadData.Male != null)
-                {
-                    needsPatching |= raceRecord.HeadData.Male.HeadParts.Any(h => vanillaToHphParts.ContainsKey(h.Head));
-                }
-                if (raceRecord.HeadData.Female != null)
-                {
-                    needsPatching |= raceRecord.HeadData.Female.HeadParts.Any(h => vanillaToHphParts.ContainsKey(h.Head));
-                }
-
-                if (needsPatching)
-                {
-                    racesToPatch.Add(raceRecord);
-                    racesThatWillBePatched.Add(raceRecord.ToLinkGetter());
-                }
-            }
-
-            foreach (var npcRecord in state.LoadOrder.PriorityOrder.OnlyEnabled().Npc().WinningOverrides())
-            {
-                if (npcRecord.EditorID == null) continue;
-
-                var needsPatching = false;
-                if (npcRecord.EditorID.EndsWith("Preset"))
-                {
-                    needsPatching = npcRecord.HeadParts.Any(h => vanillaToHphParts.ContainsKey(h));
-                }
-                else
-                {
-                    if (racesThatWillBePatched.Contains(npcRecord.Race))
-                    {
-                        needsPatching = true;
-                    }
-                }
-
-                if (needsPatching)
-                {
-                    npcsToPatch.Add(npcRecord);
-                }
-            }
-
-            return (racesToPatch, npcsToPatch);
         }
     }
 }
